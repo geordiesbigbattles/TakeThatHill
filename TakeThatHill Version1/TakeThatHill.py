85565ac1e8cb39a0752338896b1b6cad66d289d6
geordiesbigbattles <markandrewflanagan@googlemail.com>
2020-05-16 11:00:07 +0100

    Create TakeThatHill.py

---
+++ TakeThatHill Version1/TakeThatHill.py
@@ -0,0 +1,19 @@
+# "Take That Hill" Computer Assisted Wargame
+# Version 1.0
+# Mark Flanagan 16/05/2020
+# Simulation Engine
+print ("This is Take That Hill")
+# Variable Initialisation
+turns_played = 1
+# Game Block
+# Sequence of Play
+
+# End State Output
+print ()
+if turns_played == 1:
+    print ("You played", turns_played, "turn to Take That Hill")
+else:
+    print ("You played", turns_played, "turns to Take That Hill")
+print ("Well done!")
+print ()
+print ("Thank you for playing!") 
